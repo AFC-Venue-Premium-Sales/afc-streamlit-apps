bec666fab181a845010572029c430a944ec8386b
Craig Muthali <craigmunthali@live.co.uk>
2025-03-27 15:31:00 +0000

    modified:   services_portal_metrics.py

--- services_portal_metrics.py
+++ services_portal_metrics.py
@@ -6,6 +6,9 @@ import math
 from datetime import datetime
 from io import BytesIO
 
+
+st.set_page_config(page_title="Guest Portal Analysis", layout="wide")
+
 ############################################################################
 # Standardize Location Strings
 ############################################################################
