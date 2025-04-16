40749be0e7bc5a436cb8911be4bc2c6d8611327a
Craig Muthali <craigmunthali@live.co.uk>
2025-04-16 15:44:21 +0100

    modified:   guest_portal_invitations_metrics.py

--- guest_portal_invitations_metrics.py
+++ guest_portal_invitations_metrics.py
@@ -130,7 +130,7 @@ def run():
         # -------------
         # Load the preset executive boxes from the box_numbers file.
         try:
-            box_df = pd.read_excel("/Users/cmunthali/Documents/PYTHON/APPS/box_numbers.xlsx")
+            box_df = pd.read_excel("box_numbers.xlsx")
             # Ensure the columns are correctly named. Adjust if needed.
             preset_boxes = box_df["Box Number"].unique()
         except Exception as e:
